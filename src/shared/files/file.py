c1ffa67826417b5c5f2508478d4f9e7e280df4d1
cyrilclovis <cyrilclovis@gmail.com>
2025-11-11 15:43:00 +0100

    Désormais, le calcul des fichiers index, inversed_index et full_vocab est complétement délégué au tf_idf_dependencies_handler

--- src/shared/files/file.py
+++ src/shared/files/file.py
@@ -1,9 +1,13 @@
 import json
+from os.path import exists
 
 class File:
 
     def __init__(self, file_path: str):
         self._file_path = file_path
+
+    def get_path(self) -> str:
+        return self._file_path
 
     def load_text_content(self) -> list[str]:
         print(f"[READ]: Chargement du fichier {self._file_path}")
@@ -17,9 +21,6 @@ class File:
             return json.load(f)
         
     def save_json(self, data):
-        """
-        Enregistre les données sous forme de JSON dans un fichier.
-        """      
         with open(self._file_path, "w", encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
         print(f"[SAVE]: Données sauvegardées dans {self._file_path}")
@@ -27,3 +28,6 @@ class File:
         
     def get_file_name(self) -> str:
         return self._file_path.split("/")[-1]
+    
+    def exists(self) -> bool:
+        return exists(self._file_path)
--- src/tf_idf/tf_idf_dependencies_handler.py
+++ src/tf_idf/tf_idf_dependencies_handler.py
@@ -1,3 +1,5 @@
+import time
+
 from src.shared.handler.dependencies_handler import DependenciesHandler
 from src.shared.command import Command
 from src.file_handlers.file_hierarchy_enum import FileHierarchyEnum
@@ -20,32 +22,43 @@ class TfIdfDependenciesHandler(DependenciesHandler):
 
 
     def resolve_dependencies(self):
-        # 1ere type : VocabAndIndewCalulatore
-        self.get_voc_index_map()
+        self.resolve_index_inversed_index_and_full_vocab()
+        #TODO Afficher à nouveau le cadre (voir ci-dessus)
         #self.check_and_create_all("le vocabulaire, l'index et l'index inversé", self.get_voc_index_map(), self.preprocessor.name)
         # 2nd type : TfIDF
         #self.check_and_create_all("TF-IDF", self.get_tf_idf_map(), self.preprocessor.name)
         pass
 
+    def if_file_not_found_launch_calculation(self, file: File, calculation_func, *args, **kwargs):
+        if file.exists():
+            print(f"[INFO] Le fichier {file.get_file_name()} est disponible ! Voici son chemin {file.get_path()}")
+            return
+        print("-----------------")
+        print(f"[CREATION START] Le fichier {file.get_path()} n'existe pas, création en cours...")
+        self.file_handler.create_all_missing_folders(file.get_path())
+        start_time = time.time()
+        data_to_save = calculation_func(*args, **kwargs)
+        end_time = time.time()
+        print(f"[CREATION END] La création du fichier {file.get_file_name()} s'est terminée en {self.get_creation_duration_time(start_time, end_time)}!")
+        if data_to_save is None:
+            # TODO changer ce comportement là, la sauvegarde est forcément réaliser par un DependencieHandler
+            print(f"[INFO] La sauvegarde du fichier a été déléguée au fichier de calcul correspondant")
+        else:
+            file.save_json(data_to_save)
+        print("-----------------")
 
-    def get_voc_index_map(self):
-        """Retourne une carte associant les types de fichiers aux méthodes de traitement."""
-        #### INTERN DEPENDENCIES => HERE THE ORDER MATTERS !!!
-        corpus_folder = FileHierarchyEnum.get_file_path(FileHierarchyEnum.WIKI_CORPUS_FOLDER)
-        corpus_files = [File(full_path_file_name) for full_path_file_name in self.file_handler.get_full_path_files_of_folder(corpus_folder)]
 
+    def resolve_index_inversed_index_and_full_vocab(self):
+        corpus_files = [File(full_path_file) for full_path_file in self.file_handler.get_full_path_files_of_folder(FileHierarchyEnum.get_file_path(FileHierarchyEnum.WIKI_CORPUS_FOLDER))]
         index_voc_calculator = IndexAndVocabCalculator(self.preprocessor)
 
         index_file = File(self.file_handler.get_file_path(FileHierarchyEnum.INDEX, self.preprocessor.name))
         inverse_index_file = File(self.file_handler.get_file_path(FileHierarchyEnum.INVERSE_INDEX, self.preprocessor.name))
+        full_vocab_file = File(self.file_handler.get_file_path(FileHierarchyEnum.FULL_VOCAB, self.preprocessor.name))
 
-        inverse_index_file.save_json(index_voc_calculator.create_inversed_index(index_file))
-
-        return {
-            FileHierarchyEnum.INDEX:            index_voc_calculator.create_index,
-            FileHierarchyEnum.INVERSE_INDEX:    index_voc_calculator.create_inversed_index,
-            FileHierarchyEnum.FULL_VOCAB:       index_voc_calculator.extract_full_vocab,
-        }
+        self.if_file_not_found_launch_calculation(index_file, index_voc_calculator.create_index, corpus_files)
+        self.if_file_not_found_launch_calculation(inverse_index_file, index_voc_calculator.create_inversed_index, index_file)
+        self.if_file_not_found_launch_calculation(full_vocab_file, index_voc_calculator.extract_full_vocab, corpus_files)
     
     def get_tf_idf_map(self):
         """Retourne une carte associant les types de fichiers aux méthodes de traitement."""
