5e64dc3a05b9b75b25e598981db0c8ff1d814d80
okfkzksk <cyrilclovis@gmail.com>
2025-11-18 17:27:37 +0100

    deplacement de la méthode

--- src/refacto/shared/handler/dependencies_handler.py
+++ src/refacto/shared/handler/dependencies_handler.py
@@ -20,19 +20,3 @@ class DependenciesHandler(Handler):
     #@abstractmethod TODO remettre
     def resolve_dependencies(self, command: Command):
         raise NotImplementedError("This method in not implemented !")
-
-    
-    def if_file_not_found_launch_calculation(self, file: File, calculation_func, *args, **kwargs):
-        if file.exists():
-            print(f"[INFO] Le fichier {file.get_file_name()} est disponible ! Voici son chemin {file.get_path()}")
-            return
-        print("-----------------")
-        print(f"[CREATION START] Le fichier {file.get_path()} n'existe pas, création en cours...")
-        file.create_all_missing_folders() # TODO, délégué cette méthode 
-        data_to_save = calculation_func(*args, **kwargs)
-        if data_to_save is None:
-            # TODO changer ce comportement là, la sauvegarde est forcément réaliser par un DependencieHandler
-            print(f"[INFO] La sauvegarde du fichier a été déléguée au fichier de calcul correspondant")
-        else:
-            file.save(data_to_save) # TODO, On addresse désormais ce problème
-        print("-----------------")
