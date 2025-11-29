4934c98fb28d6ab640cbf77e0e3b9d0ee0b92464
cyrilclovis <cyrilclovis@gmail.com>
2025-11-29 22:45:39 +0100

    Correction resolve.parent après déplacement du fichier paths.py

--- src/back/shared/config/paths.py
+++ src/back/shared/config/paths.py
@@ -7,7 +7,7 @@ from pathlib import Path
 #│   ├── /output
 
 
-BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parents[4]
 
 # First level
 DATA_DIR = BASE_DIR / "data"
