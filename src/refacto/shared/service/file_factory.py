a7acd624a325af1ee92f131cbbc469e857936fa9
cyrilclovis <cyrilclovis@gmail.com>
2025-11-29 22:20:25 +0100

    le dossier back/config passe dans back/shared/config

--- src/refacto/shared/service/file_factory.py
+++ src/refacto/shared/service/file_factory.py
@@ -1,4 +1,4 @@
-from src.back.config.paths import OUTPUT_DIR, WIKI_CORPUS_DIR
+from src.back.shared.config.paths import OUTPUT_DIR, WIKI_CORPUS_DIR
 from os import listdir
 from os.path import join
 
